765af68c80555bb3318874896fcd598bff3c4e82
John Owen <john.owen@dabble.com.au>
2024-06-19 10:33:39 +1000

    touchpad support

--- scripts/camera_pivot.gd
+++ scripts/camera_pivot.gd
@@ -1,7 +1,8 @@
 class_name CameraPivot
 extends Node3D
 
-const ORBIT_SPEED := 0.004
+const MOUSE_ORBIT_SPEED := 0.004
+const PAN_ORBIT_SPEED := MOUSE_ORBIT_SPEED * 5.0
 const MOUSE_SENSITIVITY_MULTIPLIER := 0.001
 @onready var camera: Camera3D = $Camera
 @onready var aim: RayCast3D = $Camera/Aim
@@ -32,24 +33,33 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
+	if event is InputEventMagnifyGesture:
+		var e := event as InputEventMagnifyGesture
+		camera.position.z *= e.factor
 	if event is InputEventMouseButton:
 		var button := event as InputEventMouseButton
 		var scroll_speed := 1.5
 		if button.button_index == MOUSE_BUTTON_WHEEL_UP:
 			camera.position.z *= scroll_speed
-			if camera.position.z < -100.0:
-				camera.position.z = -100.0
 		elif button.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			camera.position.z /= scroll_speed
-			if camera.position.z > -3.0:
-				camera.position.z = -2.0
+
+	if camera.position.z < -100.0:
+		camera.position.z = -100.0
+	if camera.position.z > -3.0:
+		camera.position.z = -2.0
+
 	if event is InputEventMouseMotion:
+		var e := event as InputEventMouseMotion
 		var invert := -1.0 if g.invert_mouse else 1.0
-		var motion := event as InputEventMouseMotion
 		if fight_mode:
 			var s := g.mouse_sensitivity * MOUSE_SENSITIVITY_MULTIPLIER
-			view_pitch += motion.relative.y * s * invert
-			view_yaw -= motion.relative.x * s
+			view_pitch += e.relative.y * s * invert
+			view_yaw -= e.relative.x * s
 		elif Input.is_action_pressed("orbit"):
-			view_pitch += motion.relative.y * ORBIT_SPEED * invert
-			view_yaw -= motion.relative.x * ORBIT_SPEED
+			view_pitch += e.relative.y * MOUSE_ORBIT_SPEED * invert
+			view_yaw -= e.relative.x * MOUSE_ORBIT_SPEED
+	if event is InputEventPanGesture:
+		var e := event as InputEventPanGesture
+		view_pitch += e.delta.y * PAN_ORBIT_SPEED * -1.0
+		view_yaw -= e.delta.x * PAN_ORBIT_SPEED * -1.0
