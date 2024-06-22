21d753da740560233000d16ca77737819de110db
John Owen <john.owen@dabble.com.au>
2024-06-22 17:05:45 +1000

    better looking armor part

--- scripts/armor_part.gd
+++ scripts/armor_part.gd
@@ -1,6 +1,17 @@
 class_name ArmorPart
 extends Node3D
 
-@onready var armor: MeshInstance3D = $Armor
+@onready var armor: Node3D = $Armor
 @onready var frame: Node3D = $Frame
 var health := 100.0
+
+
+func _ready() -> void:
+	var mesh: MeshInstance3D = armor.get_node("Armor")
+	var mat: ShaderMaterial = mesh.material_override
+	var uv1 := Vector3(
+		randf_range(0.0, 1.0),
+		randf_range(0.0, 1.0),
+		randf_range(0.0, 1.0)
+	);
+	mat.set_shader_parameter("uv1_offset", uv1)
